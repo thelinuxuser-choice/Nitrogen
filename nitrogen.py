1dcfa9662ce1cc0098d623c1f509f2eb7fc858ae
Subodha Prabash <73874882+thelinuxuser-choice@users.noreply.github.com>
2021-04-29 17:48:06 +0530

    🔒

--- nitrogen.py
+++ nitrogen.py
@@ -1,60 +1,52 @@
 import random, string
 import requests
 import os
+from colorama import Fore
 
 
-FAIL = '\033[91m' #just shut up
-WARNING = '\033[93m' #nothing
 
-#Idea and help by stackoverflow
+# Idea and help by stackoverflow
 
+print(f"{Fore.RED} ▐ ▄ ▪  ▄▄▄▄▄▄▄▄             ▄▄ • ▄▄▄ . ▐ ▄  ")
+print(f"{Fore.RED}•█▌▐███ •██  ▀▄ █·▪         ▐█ ▀ ▪▀▄.▀·•█▌▐█   {Fore.YELLOW} version    :1.1")
+print(f"{Fore.RED}▐█▐▐▌▐█· ▐█.▪▐▀▀▄  ▄█▀▄     ▄█ ▀█▄▐▀▀▪▄▐█▐▐▌   {Fore.YELLOW} status     :working")
+print(f"{Fore.RED}██▐█▌▐█▌ ▐█▌·▐█•█▌▐█▌.▐▌    ▐█▄▪▐█▐█▄▄▌██▐█▌   {Fore.YELLOW} maintaining:yes")
+print(f"{Fore.RED}▀▀ █▪▀▀▀ ▀▀▀ .▀  ▀ ▀█▄▀▪    ·▀▀▀▀  ▀▀▀ ▀▀ █▪   ")
 
-print(FAIL +" ███▄    █  ██▓▄▄▄█████▓ ██▀███   ▒█████  ")
-print(FAIL +" ██ ▀█   █ ▓██▒▓  ██▒ ▓▒▓██ ▒ ██▒▒██▒  ██▒")
-print(FAIL +"▓██  ▀█ ██▒▒██▒▒ ▓██░ ▒░▓██ ░▄█ ▒▒██░  ██▒")
-print(FAIL +"▓██▒  ▐▌██▒░██░░ ▓██▓ ░ ▒██▀▀█▄  ▒██   ██░")
-print(FAIL +"▒██░   ▓██░░██░  ▒██▒ ░ ░██▓ ▒██▒░ ████▓▒░")
-print(FAIL +"░ ▒░   ▒ ▒ ░▓    ▒ ░░   ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ")
-print(FAIL +"░ ░░   ░ ▒░ ▒ ░    ░      ░▒ ░ ▒░  ░ ▒ ▒░ ")
-print(FAIL +"   ░   ░ ░  ▒ ░  ░        ░░   ░ ░ ░ ░ ▒  ")
-print(WARNING +"        ░  ░              ░         ░ ░  \n")
-                                          
+print(f"{Fore.CYAN}▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄")
+print(f"{Fore.CYAN}▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌")
+print(f"{Fore.CYAN}▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀")
+
+print(f"{Fore.GREEN}[!]github - https://github.com/thelinuxuser-choice/Nitrogen/")
 
 
-print(WARNING +"  ▄████ ▓█████  ███▄    █ ")
-print(WARNING +" ██▒ ▀█▒▓█   ▀  ██ ▀█   █ ")
-print(WARNING +"▒██░▄▄▄░▒███   ▓██  ▀█ ██▒")
-print(WARNING +"░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒")
-print(WARNING +"░▒▓███▀▒░▒████▒▒██░   ▓██░")
-print(WARNING +" ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒ ")
-print(WARNING +"  ░   ░  ░ ░  ░░ ░░   ░ ▒░")
-print(WARNING +"░ ░   ░    ░      ░   ░ ░ ")
-print(WARNING +"      ░    ░  ░         ░ \n")
-                          
+
+
+
 from time import sleep
 import sys
-line_1 = "Discord nitrogen by thelinux-userchoice\n"
+
+line_1 = "[#]Discord nitrogen by thelinux-userchoice\n"
 for x in line_1:
     print(x, end='')
     sys.stdout.flush()
     sleep(0.1)
 
+num = input(f'{Fore.MAGENTA}[?]How many nitro codes you want me to generate:')
 
-num=input('[?]How many nitro codes you want me to generate:')
-
-f=open("Generatednitro.txt","a+", encoding='utf-8')
+f = open("Generatednitro.txt", "a+", encoding='utf-8')
 
 print("")
-      
+
 for n in range(int(num)):
-   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
-   f.write('https://discord.gift/')
-   f.write(y)
-   f.write("\n")
+    y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+    f.write('https://discord.gift/')
+    f.write(y)
+    f.write("\n")
 
 f.close()
 
-print(f"[>] Generated {num} nitro codes and saved \n[^]now going to check {num} nitro codes are valid are not! ")
+print(f"{Fore.LIGHTYELLOW_EX}[>] Generated {num} nitro codes and saved \n[^]now going to check {num} nitro codes are valid are not! ")
 
 with open("Generatednitro.txt") as f:
     for line in f:
@@ -65,13 +57,12 @@ with open("Generatednitro.txt") as f:
         r = requests.get(url)
 
         if r.status_code == 200:
-            print(" [+]VALID CODE ┇ {} ".format(line.strip("\n")))
+            print("{Fore.RED}wow")
+            print("[+]VALID CODE ┇ {} ".format(line.strip("\n")))
             break
         else:
-        	print(" [-]INVALID CODE ┇ {} ".format(line.strip("\n")))
-
+            print("[-]INVALID CODE ┇ {} ".format(line.strip("\n")))
 
 os.remove("Generatednitro.txt")
 
-
-print("\nbye bye :) please give me a star \nhttps://github.com/thelinuxuser-choice/Nitrogen/")
+print(f"{Fore.LIGHTYELLOW_EX}\nbye bye :) please give me a star \n")
